52c7db7ec3008ee33a7f56ef5bfd5ff2308e86d2
Austin Kregel <austin@kregel.co>
2020-07-15 18:41:26 -0400

    Update config

--- config/app.php
+++ config/app.php
@@ -164,6 +164,8 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
 
+        Bugsnag\BugsnagLaravel\BugsnagServiceProvider::class,
+
         /*
          * Application Service Providers...
          */
