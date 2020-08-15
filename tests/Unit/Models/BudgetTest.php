dcd928e0d7802076f75060631897b0cc12c25fb5
Austin Kregel <austin@kregel.co>
2020-08-14 23:46:09 -0400

    Add data provider for tests so we can verify the logic is accurate.

--- tests/Unit/Models/BudgetTest.php
+++ tests/Unit/Models/BudgetTest.php
@@ -8,18 +8,31 @@ use Tests\TestCase;
 
 class BudgetTest extends  TestCase
 {
-    public function testGetStartOfCurrentPeriod()
+    /**
+     * @dataProvider dateDataProvider
+     */
+    public function testGetStartOfCurrentPeriod($interval, $freq, $startedAt, $startOfPeriod, $endOfPeriod)
     {
         Carbon::setTestNow(
             Carbon::create(2020, 1, 15, 0, 0, 0)
         );
         $budget = new Budget([
-            'frequency' => 'MONTHLY',
-            'interval' => 1,
-            'started_at' => Carbon::create(2020, 1, 1, 0, 0, 0),
+            'frequency' => $freq,
+            'interval' => $interval,
+            'started_at' => $startedAt,
         ]);
 
-        $this->assertSame('2020-01-01', $budget->getStartOfCurrentPeriod()->format('Y-m-d'));
-        $this->assertSame('2020-01-31', $budget->getEndOfCurrentPeriod()->format('Y-m-d'));
+        $this->assertSame($startOfPeriod, $budget->getStartOfCurrentPeriod()->format('Y-m-d'));
+        $this->assertSame($endOfPeriod, $budget->getEndOfCurrentPeriod()->format('Y-m-d'));
+    }
+
+    public function dateDataProvider()
+    {
+        return [
+            [1, 'MONTHLY', Carbon::create(2020, 1, 1, 0, 0, 0), '2020-01-01', '2020-01-31'],
+            [2, 'MONTHLY', Carbon::create(2020, 1, 1, 0, 0, 0), '2020-01-01', '2020-02-29'],
+            [1, 'YEARLY', Carbon::create(2020, 1, 1, 0, 0, 0), '2020-01-01', '2021-01-31'],
+            [2, 'WEEKLY', Carbon::create(2020, 1, 1, 0, 0, 0), '2020-01-01', '2021-01-14'],
+        ];
     }
 }
