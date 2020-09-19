2cc16b2963c127addc3eff4d41642d522cba3eee
Austin Kregel <austin@kregel.co>
2020-09-18 22:45:57 -0400

    Clean up the controoller

--- app/Http/Controllers/Plaid/CreateLinkTokenController.php
+++ app/Http/Controllers/Plaid/CreateLinkTokenController.php
@@ -3,17 +3,12 @@ namespace App\Http\Controllers\Plaid;
 
 use App\Contracts\Services\PlaidServiceContract;
 use App\Http\Controllers\Controller;
-use App\Services\Banking\PlaidService;
-use App\User;
 use Illuminate\Http\Request;
-use Psr\Log\LoggerInterface;
 
 class CreateLinkTokenController extends Controller
 {
     public function __invoke(Request $request, PlaidServiceContract $plaid)
     {
-        $this->validate($request, []);
-
         return $plaid->createLinkToken($request->user()->id);
     }
 }
