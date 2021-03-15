2a72bd5acf3c7e714d994ed4c4ffc76eb14a4b54
Austin Kregel <austin@kregel.co>
2021-03-14 20:33:59 -0400

    Add new job to delete transactions

---
+++ app/Jobs/RemoveTransactionsJob.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Account;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class RemoveTransactionsJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected Account $account;
+
+    protected array $transactionIds;
+
+    public function __construct(Account $account, array $transactionIds)
+    {
+        $this->account = $account;
+        $this->transactionIds = $transactionIds;
+    }
+
+    public function handle(): void
+    {
+        $transactions = $this->account->transactions()
+            ->whereIn('transaction_id', $this->transactionIds)
+            ->get();
+
+        $transactions->map->delete();
+    }
+}
