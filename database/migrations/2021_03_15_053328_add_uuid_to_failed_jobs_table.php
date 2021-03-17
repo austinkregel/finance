8dba6dd3659f16a801908cf8c4ee11d6e59b50fc
Austin Kregel <austin@kregel.co>
2021-03-17 19:22:45 -0400

    Add failed jobs migration update'

---
+++ database/migrations/2021_03_15_053328_add_uuid_to_failed_jobs_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddUuidToFailedJobsTable extends Migration
+{
+    public function up()
+    {
+        Schema::table('failed_jobs', function (Blueprint $table) {
+            $table->string('uuid')->after('id')->nullable()->unique();
+        });
+
+        DB::table('failed_jobs')->whereNull('uuid')->cursor()->each(function ($job) {
+            DB::table('failed_jobs')
+                ->where('id', $job->id)
+                ->update(['uuid' => (string) Str::uuid()]);
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('failed_jobs', function (Blueprint $table) {
+            $table->dropColumn('uuid');
+        });
+    }
+}
